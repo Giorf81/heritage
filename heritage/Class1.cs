fdab125ee9ec75c419674653f2b63fd21a7a7581
Giorf81 <rejroberts7@gmail.com>
2024-03-08 15:44:40 +0300

    Добавьте файлы проекта.

---
+++ heritage/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace heritage
+{
+    public class Class1
+    {
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        {
+            public string ProjectName;
+        }
+
+        class Developer: Employee
+        {
+            private string ProgrammingLanguage;
+        }
+
+    }
+}
